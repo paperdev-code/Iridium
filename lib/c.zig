16c71644debb4e7f97d3454329f63ae8be364666
paperdev <jornveken2@gmail.com>
2022-04-17 17:57:18 +0200

    C bindings package

---
+++ lib/c.zig
@@ -0,0 +1,8 @@
+usingnamespace @cImport({
+    @cInclude("glad/gl.h");
+    @cInclude("GLFW/glfw3.h");
+    @cDefine("CIMGUI_DEFINE_ENUMS_AND_STRUCTS", "1");
+    @cInclude("cimgui.h");
+    @cInclude("imgui_impl_glfw.h");
+    @cInclude("imgui_impl_opengl3.h");
+});
